1d9ab3a24b71fbf5bb3fa6cf23a05d5f8e2922e7
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-23 11:50:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,8 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   return array.push(element)
 }
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
